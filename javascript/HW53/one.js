4af34ad07e8ca1aace8e661b32963d3eb0d9a49e
DavidECohen <edcatbmg@gmail.com>
2020-07-02 22:01:05 -0400

    homework53

---
+++ javascript/HW53/one.js
@@ -0,0 +1,4 @@
+' use strict ';
+
+var name = "Eliezer Dovid Cohen";
+var email = "edcatbmg@gmail.com";
---
+++ javascript/HW53/two.js
@@ -0,0 +1,7 @@
+' use strict ';
+console.log(name);
+console.log(email);
+
+var Age = prompt('What is your age ? ');
+console.log('I am:',
+    Age, 'old');
